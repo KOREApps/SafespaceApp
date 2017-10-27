77ebd504815d749f221379672992f18c6373b8fa
erikhny <erikhny@gmail.com>
2017-10-27 14:34:38 +0200

    rofl

---
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/DocumentActivity.java
@@ -0,0 +1,15 @@
+package kore.ntnu.no.safespace.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kore.ntnu.no.safespace.R;
+
+public class DocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document);
+    }
+}
