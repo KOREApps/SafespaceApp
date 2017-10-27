4a541320d593260c0ab49e9de38d4ba718f1eb02
erikhny <erikhny@gmail.com>
2017-10-27 15:01:49 +0200

    tollebar xd

--- app/src/main/java/kore/ntnu/no/safespace/Activities/MainActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/MainActivity.java
@@ -2,6 +2,9 @@ package kore.ntnu.no.safespace.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import kore.ntnu.no.safespace.R;
 
@@ -11,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
