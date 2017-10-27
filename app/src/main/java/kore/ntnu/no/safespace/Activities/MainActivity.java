c31a2b59255e4caf1506bb1076615943b4edf8e9
oscarwika <oscar_wika@hotmail.com>
2017-10-27 17:32:08 +0200

    Removed the actionbar from login screen (added android:theme in AndroidManifest.xml)

--- app/src/main/java/kore/ntnu/no/safespace/Activities/MainActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/MainActivity.java
@@ -1,11 +1,16 @@
 package kore.ntnu.no.safespace.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import kore.ntnu.no.safespace.R;
 
@@ -16,9 +21,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText loginUser = (EditText) findViewById(R.id.launch_username);
+        EditText loginPwd = (EditText) findViewById(R.id.launch_password);
+        Button loginButton = (Button) findViewById(R.id.launch_loginButton);
+        loginButton.setOnClickListener((View v) -> {
+            // Check input -> login -> start ny activity
+            String username = loginUser.getText().toString();
+            String password = loginPwd.getText().toString();
+            Intent intent = new Intent(MainActivity.this, DocumentActivity.class);
+            startActivity(intent);
+        });
+
 
     }
-
-
-
 }
