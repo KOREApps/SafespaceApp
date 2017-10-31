7b3091d33b82868a4b21e7e4cfba6821873a76ff
oscarwika <oscar_wika@hotmail.com>
2017-10-31 16:01:29 +0100

    new class and xml for latest/previous/recent reports, permission for gps, dont have android phone rip

---
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/LatestReportActivity.java
@@ -0,0 +1,21 @@
+package kore.ntnu.no.safespace.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kore.ntnu.no.safespace.R;
+
+/**
+ * Created by OscarWika on 31.10.2017.
+ */
+
+public class LatestReportActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latestreport);
+
+
+    }
+}
