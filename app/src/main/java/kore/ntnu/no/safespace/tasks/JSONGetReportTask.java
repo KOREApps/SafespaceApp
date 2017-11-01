e189a89bdc81aba94a57f081705d077b98cef04b
oscarwika <oscar_wika@hotmail.com>
2017-11-01 11:21:28 +0100

    JSON tasks

---
+++ app/src/main/java/kore/ntnu/no/safespace/tasks/JSONGetReportTask.java
@@ -0,0 +1,31 @@
+package kore.ntnu.no.safespace.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by OscarWika on 01.11.2017.
+ */
+
+public class JSONGetReportTask extends AsyncTask<String, String, String> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+}
---
+++ app/src/main/java/kore/ntnu/no/safespace/tasks/JSONReportTask.java
@@ -0,0 +1,30 @@
+package kore.ntnu.no.safespace.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by OscarWika on 01.11.2017.
+ */
+
+public class JSONReportTask extends AsyncTask<String, String, String> {
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
