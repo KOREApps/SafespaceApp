ec8e5cf5f6472f3b6fd805f7ed8b4dd702d88174
Kristoffer <Kristoffer_rogne@hotmail.com>
2017-11-01 15:13:46 +0100

    now takes pictures with built in camera app

--- app/src/main/java/kore/ntnu/no/safespace/Activities/ReportActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/ReportActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,15 +15,19 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import kore.ntnu.no.safespace.R;
 
 public class ReportActivity extends AppCompatActivity {
 
     public static final String PICTURE_ID = "kore.ntnu.safespace.PICTURE_ID";
-    public static final int TAKE_PICTURE_REQUEST = 0;
+    public static final int TAKE_PICTURE_REQUEST = 1;
+    private File imageFile;
     private ImageView takenPhoto;
     private Bitmap bitmap;
 
@@ -45,23 +51,45 @@ public class ReportActivity extends AppCompatActivity {
     }
 
     private void takePhoto() {
-        Intent intent = new Intent(ReportActivity.this, TakePictureActivity.class);
-        intent.putExtra(PICTURE_ID, "report");
-        startActivityForResult(intent,TAKE_PICTURE_REQUEST);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            imageFile = null;
+            try {
+                imageFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (imageFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "no.ntnu.kore.fileprovider",
+                        imageFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Report_" + timeStamp + ".jpg";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = new File(storageDir, imageFileName);
+        image.createNewFile();
+        return image;
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == TAKE_PICTURE_REQUEST){
             if(resultCode == RESULT_OK){
-                String filePath = data.getStringExtra(PICTURE_ID);
-                    if (bitmap != null) {
-                        bitmap.recycle();
-                    }
-                    bitmap = BitmapFactory.decodeFile(filePath);
-                    takenPhoto.setImageBitmap(bitmap);
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+                bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+                takenPhoto.setImageBitmap(bitmap);
 
-
-                Toast.makeText(this, filePath, Toast.LENGTH_SHORT).show();
             }
         }
     }
