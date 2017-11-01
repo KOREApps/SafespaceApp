a2c66489ed7924c224265dc7c29e4f69b1263a21
oscarwika <oscar_wika@hotmail.com>
2017-11-01 12:32:22 +0100

    GPSActivity and some small changes

---
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/GPSActivity.java
@@ -0,0 +1,23 @@
+package kore.ntnu.no.safespace.Activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import kore.ntnu.no.safespace.R;
+
+/**
+ * Created by OscarWika on 01.11.2017.
+ */
+
+public class GPSActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps);
+
+
+    }
+}
