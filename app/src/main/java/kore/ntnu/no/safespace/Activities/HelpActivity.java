d70170f670105d5dafb32f9f0f91d8afb0bbb675
erikhny <erikhny@gmail.com>
2017-11-01 10:21:59 +0100

    Fiksa MainNavigationMenu xml

---
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/HelpActivity.java
@@ -0,0 +1,15 @@
+package kore.ntnu.no.safespace.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kore.ntnu.no.safespace.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
