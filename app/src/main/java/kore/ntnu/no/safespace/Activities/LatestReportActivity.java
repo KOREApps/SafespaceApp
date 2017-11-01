d0e6c7b43b6d699c77dd0b0b3754d5fc5882c732
erikhny <erikhny@gmail.com>
2017-11-01 15:24:15 +0100

    Satt opp recyclerview med adapter og heile pakka

--- app/src/main/java/kore/ntnu/no/safespace/Activities/LatestReportActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/LatestReportActivity.java
@@ -2,7 +2,10 @@ package kore.ntnu.no.safespace.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import kore.ntnu.no.safespace.Adapters.DocumentationAdapter;
 import kore.ntnu.no.safespace.R;
 
 /**
@@ -10,11 +13,18 @@ import kore.ntnu.no.safespace.R;
  */
 
 public class LatestReportActivity extends AppCompatActivity {
+    DocumentationAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_latestreport);
+
+        adapter = new DocumentationAdapter(this);
+
+        RecyclerView rv = findViewById(R.id.rv_reports);
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rv.setAdapter(adapter);
 
 
     }
