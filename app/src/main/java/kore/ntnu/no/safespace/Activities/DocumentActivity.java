af3e5ee803b13c3f7e83245ba613325ca77d5991
Kristoffer <Kristoffer_rogne@hotmail.com>
2017-11-08 12:39:47 +0100

    small fixes

--- app/src/main/java/kore/ntnu/no/safespace/Activities/DocumentActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/DocumentActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import kore.ntnu.no.safespace.Data.Documentation;
 import kore.ntnu.no.safespace.R;
 
 public class DocumentActivity extends AppCompatActivity {
+    private Documentation doc;
+    private TextView title;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,7 @@ public class DocumentActivity extends AppCompatActivity {
 
         Spinner dropDown = findViewById(R.id.docProject);
         String[] projects = new String[]{"Robert blir ferdig", "Kristoffer får bank", "Oskar døde"}; //TODO erstatt dummy data med data fra server
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, projects);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, projects);
         dropDown.setAdapter(adapter);
 
     }
