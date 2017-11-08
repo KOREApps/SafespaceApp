d599644fc78fbdbbcf5247c26fd964f759fc7c9c
Erik <erikhny@gmail.com>
2017-11-08 13:28:12 +0100

    Listener på report bug popup

--- app/src/main/java/kore/ntnu/no/safespace/Activities/HelpActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/HelpActivity.java
@@ -9,11 +9,16 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
 
+import kore.ntnu.no.safespace.Data.BugReport;
 import kore.ntnu.no.safespace.R;
 
 public class HelpActivity extends AppCompatActivity {
+    Button RepBugBtn;
+    EditText bugTitleView;
+    EditText bugDescriptionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class HelpActivity extends AppCompatActivity {
 
         Button bugBtn = findViewById(R.id.bugReportBtn);
 
+
         bugBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,7 +35,9 @@ public class HelpActivity extends AppCompatActivity {
             }
         });
 
-        //TODO listener til popup report knapp og funksjonalitet.
+
+
+
     }
 
     public void onButtonShowPopupWindow(View view) {
@@ -49,6 +57,25 @@ public class HelpActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 popupWindow.dismiss();
                 return true;
+            }
+        });
+        RepBugBtn = popupView.findViewById(R.id.bugReportSendBtn);
+        bugDescriptionView = popupView.findViewById(R.id.bugReportDescription);
+        bugTitleView = popupView.findViewById(R.id.bugReportTitle);
+        RepBugBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String bugTitle = bugTitleView.getText().toString();
+                String bugDescription = bugDescriptionView.getText().toString();
+                //TODO sett in User
+                if (!bugTitle.equals("") && !bugDescription.equals("")) {
+                    BugReport report = new BugReport(bugTitle, bugDescription, null);
+                    System.out.println(bugTitle + bugDescription + report);
+                } else {
+                    //TODO Do something
+                    System.out.println("Lulllll");
+                }
+
             }
         });
 
