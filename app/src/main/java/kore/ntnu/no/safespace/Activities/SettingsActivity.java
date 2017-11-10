0791960b1cc794b9a8cfe6cbd0d3aeedfb24a37f
erikhny <erikhny@gmail.com>
2017-11-10 12:46:46 +0100

    La til en loggut knapp i settings.

--- app/src/main/java/kore/ntnu/no/safespace/Activities/SettingsActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/Activities/SettingsActivity.java
@@ -1,7 +1,10 @@
 package kore.ntnu.no.safespace.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import kore.ntnu.no.safespace.R;
 
@@ -11,5 +14,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        TextView logOut = findViewById(R.id.tv_logout);
+
+        logOut.setOnClickListener(view -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
