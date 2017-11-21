c698b8e56d5753977c66ff6bc55af9204b9f3e4d
Kristoffer <Kristoffer_rogne@hotmail.com>
2017-11-21 11:09:44 +0100

    cleaner executiong of backgroudnthread regarding sending in reports to database

--- app/src/main/java/kore/ntnu/no/safespace/tasks/InternetConnectionThread.java
+++ app/src/main/java/kore/ntnu/no/safespace/tasks/InternetConnectionThread.java
@@ -1,10 +1,12 @@
 package kore.ntnu.no.safespace.tasks;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Environment;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import kore.ntnu.no.safespace.data.Documentation;
@@ -18,6 +20,7 @@ import kore.ntnu.no.safespace.utils.StorageUtils;
 
 public class InternetConnectionThread extends Thread {
     private Context context;
+    private List<AsyncTask> tasks = new ArrayList<>();
 
     public InternetConnectionThread(Context context) {
         this.context = context;
@@ -34,36 +37,49 @@ public class InternetConnectionThread extends Thread {
             }
         } else {
             while (ConnectionUtil.isConnected(context)) {
-                //TODO: send Images.
-                try {
-                    List<File> docs = StorageUtils.getDocumentations(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS));
-                    if (!docs.isEmpty()) {
-                        for (File docFile : docs) {
-                            Documentation doc = StorageUtils.readDocumentFromFile(docFile);
-                            SendDocumentationTask sdt = new SendDocumentationTask(result -> {
-                                System.out.println("Sent");
-                                if (result != null) {
-                                    StorageUtils.removeReport(doc, docFile);
-                                }
-                            });
-                            sdt.execute(doc);
-                            System.out.println("Executed");
+                if (tasks.size() == 0) {
+                    //TODO: send Images.
+                    try {
+                        List<File> docs = StorageUtils.getDocumentations(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS));
+                        if (!docs.isEmpty()) {
+                            for (File docFile : docs) {
+                                Documentation doc = StorageUtils.readDocumentFromFile(docFile);
+                                SendDocumentationTask sdt = new SendDocumentationTask(result -> {
+                                    if (result != null) {
+                                        StorageUtils.removeReport(doc, docFile);
+                                    }
+                                });
+                                tasks.add(sdt);
+                                sdt.execute(doc);
+                            }
                         }
-                    }
-                    List<File> reports = StorageUtils.getIncidents(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS));
-                    if (!reports.isEmpty()) {
-                        for (File report : reports) {
-                            IncidentReport incident = StorageUtils.readIncidentFromFile(report);
-                            new SendReportTask(result -> {
-                                if (result != null) {
-                                    StorageUtils.removeReport(incident, report);
-                                }
-                            }).execute(incident);
+                        List<File> reports = StorageUtils.getIncidents(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS));
+                        if (!reports.isEmpty()) {
+                            for (File report : reports) {
+                                IncidentReport incident = StorageUtils.readIncidentFromFile(report);
+                                SendReportTask srt = new SendReportTask(result -> {
+                                    if (result != null) {
+                                        StorageUtils.removeReport(incident, report);
+                                    }
+                                });
+                                tasks.add(srt);
+                                srt.execute(incident);
+                            }
                         }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
                     }
-                    sleep(300000);
-                } catch (IOException | ClassNotFoundException | InterruptedException e) {
-                    e.printStackTrace();
+                } else {
+                    try {
+                        for(AsyncTask task : tasks){
+                            if(task.getStatus() == AsyncTask.Status.FINISHED){
+                                tasks.remove(task);
+                            }
+                        }
+                        sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
