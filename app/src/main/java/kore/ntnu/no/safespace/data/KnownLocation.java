7748dc71465e7408b8bb2575febcdb9f9e3b7352
Robert R <rrishaugr@gmail.com>
2017-11-21 20:01:51 +0100

    Added KnownLocationService and KnownLocation class

---
+++ app/src/main/java/kore/ntnu/no/safespace/data/KnownLocation.java
@@ -0,0 +1,44 @@
+package kore.ntnu.no.safespace.data;
+
+/**
+ * Created by robert on 11/21/17.
+ */
+
+public class KnownLocation {
+
+    private Long id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private Integer radius;
+
+    public KnownLocation() {}
+
+    public KnownLocation(Long id, String name, Double latitude, Double longitude, Integer radius) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Integer getRadius() {
+        return radius;
+    }
+}
