726fde454ab4b8bd38fa269e6c6ae4ba4e6c54d6
oscarwika <oscar_wika@hotmail.com>
2017-11-23 01:16:41 +0100

    Updates to map activity - forgot to push

--- app/src/main/java/kore/ntnu/no/safespace/RegisterLocationActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/RegisterLocationActivity.java
@@ -3,9 +3,14 @@ package kore.ntnu.no.safespace;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import kore.ntnu.no.safespace.service.KnownLocationService;
+import kore.ntnu.no.safespace.tasks.RegisterNewLocationTask;
 import kore.ntnu.no.safespace.utils.IdUtils;
 
 public class RegisterLocationActivity extends AppCompatActivity {
@@ -14,6 +19,7 @@ public class RegisterLocationActivity extends AppCompatActivity {
     private EditText locationLatText;
     private EditText locationLogText;
     private EditText locationRadius;
+    private Button registerLocationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +30,17 @@ public class RegisterLocationActivity extends AppCompatActivity {
         locationLatText = findViewById(R.id.locLatInput);
         locationLogText = findViewById(R.id.locLogInput);
         locationRadius = findViewById(R.id.locRadiusInput);
+        registerLocationBtn = findViewById(R.id.registerButton);
 
         Intent intent = getIntent();
         locationLatText.setText(Double.toString(intent.getDoubleExtra(IdUtils.MAPS_LAT, 0.0)));
         locationLogText.setText(Double.toString(intent.getDoubleExtra(IdUtils.MAPS_LOG, 0.0)));
+
+        registerLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "GG", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/kore/ntnu/no/safespace/activities/MapsActivity.java
+++ app/src/main/java/kore/ntnu/no/safespace/activities/MapsActivity.java
@@ -2,13 +2,30 @@ package kore.ntnu.no.safespace.activities;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import kore.ntnu.no.safespace.R;
 import kore.ntnu.no.safespace.RegisterLocationActivity;
@@ -17,6 +34,9 @@ import kore.ntnu.no.safespace.utils.IdUtils;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private MarkerOptions marker;
+    private EditText mapSearchField;
+    private CircleOptions circleOptions = new CircleOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +46,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mapSearchField = findViewById(R.id.searchMapField);
+        Button okBtn = findViewById(R.id.okBtn);
+        okBtn.setOnClickListener(view -> registerLocationBtn());
     }
-
-
-
-
-
 
     /**
      * Manipulates the map once available.
@@ -44,18 +63,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        init();
         mMap = googleMap;
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Intent intent = new Intent(MapsActivity.this, RegisterLocationActivity.class);
-                intent.putExtra(IdUtils.MAPS_LAT, latLng.latitude);
-                intent.putExtra(IdUtils.MAPS_LOG, latLng.longitude);
-                System.out.println("Latitude: " + latLng.latitude);
-                System.out.println("Longitude: " + latLng.longitude);
-                startActivity(intent);
-            }
+
+        LatLng alesund = new LatLng(62.47, 6.22);
+        marker = new MarkerOptions();
+        mMap.addMarker(marker.position(alesund).title("Marker in Ålesund"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(alesund, 10.0f));
+
+        circleOptions.center(alesund)
+                .radius(10000)
+                .fillColor(adjustAlpha(0x3300ffff, 0.5f))
+                .strokeWidth(4);
+        mMap.addCircle(circleOptions);
+
+        mMap.setOnMapClickListener(latLng -> {
+            System.out.println(latLng.latitude + " " + latLng.longitude);
+            mMap.clear();
+            LatLng currentPosition = new LatLng(latLng.latitude, latLng.longitude);
+            mMap.addMarker(marker.position(currentPosition).title("Where we at tho?"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 10.0f));
+            circleOptions.center(currentPosition)
+                    .radius(10000)
+                    .fillColor(adjustAlpha(0x33FFFFFF, 1f))
+                    .strokeWidth(4);
+            mMap.addCircle(circleOptions);
         });
+    }
+
+    private void registerLocationBtn() {
+        Intent intent = new Intent(MapsActivity.this, RegisterLocationActivity.class);
+        LatLng latLng = marker.getPosition();
+        intent.putExtra(IdUtils.MAPS_LAT, latLng.latitude);
+        intent.putExtra(IdUtils.MAPS_LOG, latLng.longitude);
+        System.out.println("Latitude: " + latLng.latitude);
+        System.out.println("Longitude: " + latLng.longitude);
+        startActivity(intent);
     }
 
     private int adjustAlpha(int color, float factor) {
@@ -65,4 +108,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         int blue = Color.blue(color);
         return Color.argb(alpha, red, green, blue);
     }
+
+    private void init() {
+        mapSearchField.setOnEditorActionListener((textView, i, keyEvent) -> {
+            if( i == EditorInfo.IME_ACTION_SEARCH
+                    || i == EditorInfo.IME_ACTION_DONE
+                    || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                    || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
+                findLocation();
+            }
+            return false;
+        });
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    private void findLocation() {
+        String searchString = mapSearchField.getText().toString();
+        Geocoder geoCoder = new Geocoder(MapsActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geoCoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {}
+
+        if(list.size() > 0) {
+            Address address = list.get(0);
+
+            LatLng currentPosition = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.clear();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom((currentPosition), 10.0f));
+            mMap.addMarker(marker.position(currentPosition).title(address.getAddressLine(0)));
+
+            circleOptions = new CircleOptions();
+            circleOptions.center(currentPosition)
+                    .radius(10000)
+                    .fillColor(adjustAlpha(0x33FFFFFF, 1f))
+                    .strokeWidth(4);
+            mMap.addCircle(circleOptions);
+        }
+        hideKeyboard();
+    }
+
+    private void setRadius() {
+    }
+
+
 }
