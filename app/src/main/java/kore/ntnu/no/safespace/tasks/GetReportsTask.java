5315af7f9d6c44b43a93b77ef301b1f9028e83b8
Robert R <rrishaugr@gmail.com>
2017-11-26 15:42:01 +0100

    Fix for images not loading on reports in latest reports activity

--- app/src/main/java/kore/ntnu/no/safespace/tasks/GetReportsTask.java
+++ app/src/main/java/kore/ntnu/no/safespace/tasks/GetReportsTask.java
@@ -3,14 +3,20 @@ package kore.ntnu.no.safespace.tasks;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import kore.ntnu.no.safespace.data.Image;
 import kore.ntnu.no.safespace.data.IncidentReport;
+import kore.ntnu.no.safespace.service.ImageService;
 import kore.ntnu.no.safespace.service.ReportService;
 import kore.ntnu.no.safespace.service.ServiceResult;
+import kore.ntnu.no.safespace.utils.StorageUtils;
 
 /**
  * Class description..
@@ -21,11 +27,13 @@ public class GetReportsTask extends AsyncTask<Void, Integer, AsyncTaskResult<Lis
 
     private AsyncOnPostExecute<List<IncidentReport>> callback;
     private ReportService reportService;
+    private ImageService imageService;
     private ProgressDialog dialog;
 
     public GetReportsTask(AsyncOnPostExecute<List<IncidentReport>> callback, Context context) {
         this.callback = callback;
         this.reportService = new ReportService();
+        this.imageService = new ImageService();
         dialog = new ProgressDialog(context);
     }
 
@@ -41,6 +49,19 @@ public class GetReportsTask extends AsyncTask<Void, Integer, AsyncTaskResult<Lis
     protected AsyncTaskResult<List<IncidentReport>> doInBackground(Void... voids) {
         try {
             ServiceResult<List<IncidentReport>> serviceResult = reportService.getAll();
+            if(serviceResult != null) {
+                for (IncidentReport d : serviceResult.getObject()) {
+                    List<Image> images = new ArrayList<>();
+                    for (Image i : reportService.getImagesForReport(d.getId()).getObject()) {
+                        byte[] rawData = imageService.getImageData(i);
+                        File image = StorageUtils.saveToDisk(rawData, i.getName(), i.getFileExtension());
+                        images.add(new Image(image));
+                    }
+                    d.setImages(images);
+                }
+            } else {
+                Log.e(GetDocumentationsTask.class.getSimpleName(), "Failed to get Documentation");
+            }
             return new AsyncTaskResult<>(serviceResult.getObject());
         } catch (IOException e) {
             e.printStackTrace();
